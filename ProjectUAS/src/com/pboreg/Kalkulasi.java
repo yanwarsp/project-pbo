6389b0e2f0a1e9f2e18d84326c89fb6def19b9e8
Kevin Ope <41668520+kevinopee@users.noreply.github.com>
2020-12-23 01:26:58 +0700

    Update Kalkulasi.java

--- ProjectUAS/src/com/pboreg/Kalkulasi.java
+++ ProjectUAS/src/com/pboreg/Kalkulasi.java
@@ -30,7 +30,37 @@ public class Kalkulasi {
                 + (uas * pUas / persentase);
     }
 
-    public double hitungIp(double sks, double nilai2){
-        return nilaiAkhir2 = nilai2 / sks;
+    public double hitungIp(double nilai2){
+        if (nilai2 >= 90 && nilai2 <= 100){
+            double a = 4.0;
+            return this.nilaiAkhir2 = a;
+        } else if (nilai2 >= 80 && nilai2 <= 89.99){
+            double b = 3.7;
+            return this.nilaiAkhir2 = b;
+        } else if (nilai2 >= 75 && nilai2 <= 79.99){
+            double c = 3.3;
+            return this.nilaiAkhir2 = c;
+        } else if (nilai2 >= 70 && nilai2 <= 74.99){
+            double d = 3.0;
+            return this.nilaiAkhir2 = d;
+        } else if (nilai2 >= 65 && nilai2 <= 69.99){
+            double e = 2.7;
+            return this.nilaiAkhir2 = e;
+        } else if (nilai2 >= 60 && nilai2 <= 64.99){
+            double f = 2.3;
+            return this.nilaiAkhir2 = f;
+        } else if (nilai2 >= 55 && nilai2 <= 59.99){
+            double g = 2.0;
+            return this.nilaiAkhir2 = g;
+        } else if (nilai2 >= 50 && nilai2 <= 54.99){
+            double h = 1.7;
+            return this.nilaiAkhir2 = h;
+        } else if (nilai2 >= 40 && nilai2 <= 49.99){
+            double i = 1;
+            return this.nilaiAkhir2 = i;
+        } else {
+            double j = 0;
+            return this.nilaiAkhir2 = j;
+        }
     }
 }
